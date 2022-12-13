3abf18b7c7c4404bf2a651b9f564bbfc77cd3196
Yulia Balenko <yulia@YuliaBookPro13.local>
2022-12-12 23:23:57 -0800

    Added Bestselling Books exercise from Class 8 Homework

--- fetch.js
+++ fetch.js
@@ -4,20 +4,20 @@ const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
 
 fetch(url)
-  .then(function(data) {
-    return data.json();
-  })
-  .then(function(responseJson) {
-    console.log(responseJson);
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (responseJson) {
+        console.log(responseJson);
 
-    let article = responseJson.response.docs[0];
-    console.log(article);
+        let article = responseJson.response.docs[0];
+        console.log(article);
 
-    const mainHeadline = article.headline.main;
-    document.getElementById('article-title').innerText = mainHeadline;
+        const mainHeadline = article.headline.main;
+        document.getElementById('article-title').innerText = mainHeadline;
 
-    if (article.multimedia.length > 0) {
-      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
-      document.getElementById('article-img').src = imgUrl;
-    }
-  });
+        if (article.multimedia.length > 0) {
+            const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
+            document.getElementById('article-img').src = imgUrl;
+        }
+    });
